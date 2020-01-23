96156966dbf55ff77b7202ee757e8f17033e2830
mmxmb <lawnboymadmax@gmail.com>
2020-01-23 16:53:29 -0500

    put schema in separate dir

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-from api_schema import schema
+from api_schema.api_schema import request_schema
 from course_sched.course_sched import CourseSched, Course, Curriculum, SolverCallbackUtil
 
 
@@ -10,10 +10,10 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        data = [{'name': 'Sue', 'age': '28', 'gender': 'Squid'},
-                {'name': 'Sam', 'age': '42'},
-                {'name': 'Sacha', 'age': '20', 'gender': 'KID'}]
-        validated = schema.validate(data)
+        # data = [{'name': 'Sue', 'age': '28', 'gender': 'Squid'},
+                # {'name': 'Sam', 'age': '42'},
+                # {'name': 'Sacha', 'age': '20', 'gender': 'KID'}]
+        # validated = schema.validate(data)
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/sched')
